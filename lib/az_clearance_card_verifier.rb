1494bb4815ec95f967cf89e417c084aaba2a4aa9
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-09-11 21:53:23 -0700

    Add gem skeleton

---
+++ lib/az_clearance_card_verifier.rb
@@ -0,0 +1,5 @@
+require "az_clearance_card_verifier/version"
+
+module AzClearanceCardVerifier
+  # Your code goes here...
+end
---
+++ lib/az_clearance_card_verifier/version.rb
@@ -0,0 +1,3 @@
+module AzClearanceCardVerifier
+  VERSION = "0.1.0"
+end
